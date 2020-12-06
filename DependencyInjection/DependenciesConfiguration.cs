82d0ea9c253949060f3323a3739c22f2e8da06e0
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-12-06 22:00:48 +0300

    implemented DI config, added dependency provider

---
+++ DependencyInjection/DependenciesConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjection
+{
+    public class DependenciesConfiguration
+    {
+        private ConcurrentDictionary<Type, List<Type>> dictionary;
+
+        public DependenciesConfiguration()
+        {
+            dictionary = new ConcurrentDictionary<Type, List<Type>>();
+        }
+
+        public bool Register<TDependency, TImplementation>()
+        {
+            return Register(typeof(TDependency), typeof(TImplementation));
+        }
+
+        public bool Register(Type tDependency, Type tImplementation)
+        {
+            if (!tImplementation.IsInterface && !tImplementation.IsAbstract)
+            {
+                if (!dictionary.ContainsKey(tDependency))
+                {
+                    dictionary.TryAdd(tDependency, new List<Type>());
+                }
+                if (!dictionary[tDependency].Contains(tImplementation))
+                {
+                    dictionary[tDependency].Add(tImplementation);
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ DependencyInjection/DependencyProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjection
+{
+    public class DependencyProvider
+    {
+        private DependenciesConfiguration _configuration;
+
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
