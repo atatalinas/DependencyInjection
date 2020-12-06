1907682711698c8b3389fab84b46419baacdca73
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-12-06 21:32:16 +0300

    DI config

---
+++ DependencyInjection/DepedenciesConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DependencyInjection
+{
+    public class DependenciesConfiguration
+    {
+        private ConcurrentDictionary<Type, List<Type>> dictionary;
+
+        public DependenciesConfiguration()
+        {
+            dictionary = new ConcurrentDictionary<Type, List<Type>>();
+        }
+
+        public bool Register<TDependency, TImplementation>()
+        {
+            return Register(typeof(TDependency), typeof(TImplementation));
+        }
+
+        public bool Register(Type tDependency, Type tImplementation)
+        {
+            if (!tImplementation.IsInterface && !tImplementation.IsAbstract)
+            {
+
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
