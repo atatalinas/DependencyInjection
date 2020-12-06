6ba892b6fb7b192250dd3761a522bee8e257087c
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-12-06 22:09:52 +0300

    added validation

--- DependencyInjection/DependencyProvider.cs
+++ DependencyInjection/DependencyProvider.cs
@@ -12,5 +12,27 @@ namespace DependencyInjection
         {
             _configuration = configuration;
         }
+
+        private bool ValidateConfiguration(DependenciesConfiguration configuration)
+        {
+            foreach (Type tDependency in configuration.dictionary.Keys)
+            {
+                if (!tDependency.IsValueType)
+                {
+                    foreach (Type tImplementation in configuration.dictionary[tDependency])
+                    {
+                        if (tImplementation.IsAbstract || tImplementation.IsInterface)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
